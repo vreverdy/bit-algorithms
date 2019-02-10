dac0d01e80674128b216fab39fdf6fb882ffa027
Vincent Reverdy <vince.rev@gmail.com>
2019-02-09 17:44:50 -1000

    Original commit

---
+++ make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# =================================== MAKE =================================== #
+# Project: 			Bit Algorithms
+# Name:				make.sh
+# Description: 		Main makefile to compile different components of the project
+# Creator:         	Vincent Reverdy
+# Contributor(s):  	Vincent Reverdy [2019]
+# License:         	BSD 3-Clause License
+# ============================================================================ #
+# This file allows to compile several parts of the bit algorithms library:
+# 	- ./make.sh clean: removes temporary compilation files
+#	- ./make.sh update: updates dependencies from online repositories
+#	- ./make.sh doc: compiles documentation using latex
+# ============================================================================ #
+
+
+
+# ================================ PARAMETERS ================================ #
+
+# ============================================================================ #
