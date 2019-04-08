2a5d3f2118c284b2e4960b883e4c295ed9b200dd
Bryce Kille <kille2@illinois.edu>
2019-04-07 22:51:22 -0700

    Messy tests. Will clean up if we continue to use catch2

--- src/tests/copy.hpp
+++ src/tests/copy.hpp
@@ -3,6 +3,7 @@
 #include "bit.hpp"
 #include "catch2.hpp"
 #include "test_utils.hpp"
+#include "list"
 
 TEMPLATE_TEST_CASE("One word copy of same size", "[copy]",
   unsigned short, unsigned int, unsigned long, unsigned long long) {
@@ -56,3 +57,156 @@ TEMPLATE_TEST_CASE("One word partial copy", "[copy]",
   REQUIRE(bits_1 == expected_after_copy);
 }
 
+TEMPLATE_PRODUCT_TEST_CASE("Copy: copy to smaller words correct", 
+                           "[template][product]", 
+                           (std::vector, std::list, std::forward_list), 
+                           (unsigned int, unsigned long, unsigned long long)) {
+
+    using container_type = TestType;
+    using num_type = typename container_type::value_type;
+    auto container_size = 4;
+    auto digits = bit::binary_digits<num_type>::value;
+    container_type bitcont1 = make_random_container<container_type>
+                                     (container_size); //, 0, 0);
+    container_type bitcont2 = make_random_container<container_type, unsigned short>
+                                     (container_size*8); //, std::numeric_limits<num_type>::max(),
+                                      //std::numeric_limits<num_type>::max());
+    auto boolcont1 = bitcont_to_boolcont(bitcont1);
+    auto boolcont2 = bitcont_to_boolcont(bitcont2);
+    auto bfirst1 = bit::bit_iterator<decltype(std::begin(bitcont1))>(std::begin(bitcont1));
+    auto blast1 = bit::bit_iterator<decltype(std::end(bitcont1))>(std::end(bitcont1));
+    auto bfirst2 = bit::bit_iterator<decltype(std::begin(bitcont2))>(std::begin(bitcont2));
+    auto blast2 = bit::bit_iterator<decltype(std::end(bitcont2))>(std::end(bitcont2));
+    auto bool_first1 = std::begin(boolcont1);
+    auto bool_last1 = std::end(boolcont1);
+    auto bool_first2 = std::begin(boolcont2);
+    auto bool_last2 = std::end(boolcont2);
+    auto bool_first1_t = bool_first1;
+    auto bool_first2_t = bool_first2;
+    auto bfirst1_t = bfirst1;
+    auto bfirst2_t = bfirst2;
+    auto bool_last1_t = bool_last1;
+    auto bool_last2_t = bool_last2;
+    auto blast1_t = blast1;
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+    std::advance(bfirst1_t, 3);
+    std::advance(bool_first1_t, 3);
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+    bool_last1_t = bool_first1;
+    std::advance(bool_last1_t, (container_size-1)*digits-digits/2);
+    blast1_t = bfirst1;
+    std::advance(blast1_t, (container_size-1)*digits-digits/2);
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("Copy: copy to larger words correct", 
+                           "[template][product]", 
+                           (std::vector, std::list, std::forward_list), 
+                           (unsigned short, unsigned int, unsigned long)) {
+
+    using container_type = TestType;
+    using num_type = typename container_type::value_type;
+    auto container_size = 4;
+    auto digits = bit::binary_digits<num_type>::value;
+    container_type bitcont1 = make_random_container<container_type>
+                                     (container_size); //, 0, 0);
+    container_type bitcont2 = make_random_container<container_type, unsigned long long>
+                                     (container_size*8); //, std::numeric_limits<num_type>::max(),
+                                      //std::numeric_limits<num_type>::max());
+    auto boolcont1 = bitcont_to_boolcont(bitcont1);
+    auto boolcont2 = bitcont_to_boolcont(bitcont2);
+    auto bfirst1 = bit::bit_iterator<decltype(std::begin(bitcont1))>(std::begin(bitcont1));
+    auto blast1 = bit::bit_iterator<decltype(std::end(bitcont1))>(std::end(bitcont1));
+    auto bfirst2 = bit::bit_iterator<decltype(std::begin(bitcont2))>(std::begin(bitcont2));
+    auto blast2 = bit::bit_iterator<decltype(std::end(bitcont2))>(std::end(bitcont2));
+    auto bool_first1 = std::begin(boolcont1);
+    auto bool_last1 = std::end(boolcont1);
+    auto bool_first2 = std::begin(boolcont2);
+    auto bool_last2 = std::end(boolcont2);
+    auto bool_first1_t = bool_first1;
+    auto bool_first2_t = bool_first2;
+    auto bfirst1_t = bfirst1;
+    auto bfirst2_t = bfirst2;
+    auto bool_last1_t = bool_last1;
+    auto bool_last2_t = bool_last2;
+    auto blast1_t = blast1;
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+    std::advance(bfirst1_t, 3);
+    std::advance(bool_first1_t, 3);
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+    bool_last1_t = bool_first1;
+    std::advance(bool_last1_t, (container_size-1)*digits-digits/2);
+    blast1_t = bfirst1;
+    std::advance(blast1_t, (container_size-1)*digits-digits/2);
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+}
+
+
+TEMPLATE_PRODUCT_TEST_CASE("Copy: same size not aligned copy correct", 
+                           "[template][product]", 
+                           (std::vector, std::list, std::forward_list), 
+                           (unsigned short, unsigned int, unsigned long, unsigned long long)) {
+
+    using container_type = TestType;
+    using num_type = typename container_type::value_type;
+    auto container_size = 4;
+    auto digits = bit::binary_digits<num_type>::value;
+    container_type bitcont1 = make_random_container<container_type>
+                                     (container_size); //, 0, 0);
+    container_type bitcont2 = make_random_container<container_type>
+                                     (container_size); //, std::numeric_limits<num_type>::max(),
+                                      //std::numeric_limits<num_type>::max());
+    auto boolcont1 = bitcont_to_boolcont(bitcont1);
+    auto boolcont2 = bitcont_to_boolcont(bitcont2);
+    auto bfirst1 = bit::bit_iterator<decltype(std::begin(bitcont1))>(std::begin(bitcont1));
+    auto blast1 = bit::bit_iterator<decltype(std::end(bitcont1))>(std::end(bitcont1));
+    auto bfirst2 = bit::bit_iterator<decltype(std::begin(bitcont2))>(std::begin(bitcont2));
+    auto blast2 = bit::bit_iterator<decltype(std::end(bitcont2))>(std::end(bitcont2));
+    auto bool_first1 = std::begin(boolcont1);
+    auto bool_last1 = std::end(boolcont1);
+    auto bool_first2 = std::begin(boolcont2);
+    auto bool_last2 = std::end(boolcont2);
+    auto bool_first1_t = bool_first1;
+    auto bool_first2_t = bool_first2;
+    auto bfirst1_t = bfirst1;
+    auto bfirst2_t = bfirst2;
+    auto bool_last1_t = bool_last1;
+    auto bool_last2_t = bool_last2;
+    auto blast1_t = blast1;
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+    std::advance(bfirst1_t, 3);
+    std::advance(bool_first1_t, 3);
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+    bool_last1_t = bool_first1;
+    std::advance(bool_last1_t, (container_size-1)*digits-digits/2);
+    blast1_t = bfirst1;
+    std::advance(blast1_t, (container_size-1)*digits-digits/2);
+    copy(bfirst1_t, blast1_t, bfirst2_t);
+    std::copy(bool_first1_t, bool_last1_t, bool_first2_t);
+    REQUIRE(std::equal(bool_first2, bool_last2, bfirst2, blast2, comparator));
+
+}
