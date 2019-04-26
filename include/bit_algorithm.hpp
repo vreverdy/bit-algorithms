a8fe012c9ea412b934c22648b615b1be75a4278f
Bryce Kille <kille2@illinois.edu>
2019-04-25 17:49:46 -0700

    Header file for library

--- include/bit_algorithm.hpp
+++ include/bit_algorithm.hpp
@@ -1,16 +1,18 @@
-// ============================= BIT ALGORITHM ============================== //
+// ============================= BIT_ALGORITHM ============================== //
 // Project:         The C++ Bit Library
 // Name:            bit_algorithm.hpp
 // Description:     Optimized versions of algorithms for bit manipulation
 // Creator:         Vincent Reverdy
 // Contributor(s):  Vincent Reverdy [2015-2017]
 //                  Maghav Kumar [2016-2017]
+//                  Bryce Kille [2019]
+//                  Collin Gress [2019]
 // License:         BSD 3-Clause License
 // ========================================================================== //
-#pragma once
 // ========================================================================== //
 // ================================ PREAMBLE ================================ //
-#include "iterator_utils.hpp"
+#include "debug_utils.hpp" //TODO does this belong somewhere else?
+#include "bit_algorithm_details.hpp"
 #include "count.hpp"
 #include "count_if.hpp"
 #include "copy.hpp"
