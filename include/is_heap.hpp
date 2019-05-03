d195a932dc0b45ff97dce6e02c8e4c9aae52720c
Collin Gress <gress126@gmail.com>
2019-05-03 15:08:45 -0500

    add definitions and a few tests for is_sorted and is_heap

--- include/is_heap.hpp
+++ include/is_heap.hpp
@@ -3,7 +3,7 @@
 // Name: is_heap.hpp
 // Description: bit_iterator overloads for std::is_heap
 // Creator: Vincent Reverdy
-// Contributor(s): 
+// Contributor(s): Collin Gress [2019] 
 // License: BSD 3-Clause License
 // ========================================================================== //
 #ifndef _IS_HEAP_HPP_INCLUDED
@@ -18,35 +18,33 @@
 
 namespace bit {
 
-// TODO
 template <class RandomIt>
 constexpr bool is_heap(bit_iterator<RandomIt> first, bit_iterator<RandomIt> last) {
-    (first, last);
-    return true;
+    if (first.base() == last.base()) {
+        // even numbers cannot be heap
+        if (*first.base() % 2 == 0 && *first.base() > 0) {
+            return false;
+        }
+    }
+    return std::is_heap(first, last);
 }
 
-// TODO
 template <class ExecutionPolicy, class RandomIt>
 bool is_heap(ExecutionPolicy&& policy, bit_iterator<RandomIt> first,
     bit_iterator<RandomIt> last) {
-    (policy, first, last); 
-    return true;
+    return std::is_heap(std::forward<ExecutionPolicy>(policy), first, last);
 }
 
-// TODO
 template <class RandomIt, class Compare>
 constexpr bool is_heap(bit_iterator<RandomIt> first, bit_iterator<RandomIt> last,
     Compare comp) {
-    (first, last, comp);
-    return true;
+    return std::is_heap(first, last, comp);
 }
 
-// TODO
 template <class ExecutionPolicy, class RandomIt, class Compare>
 bool is_heap(ExecutionPolicy&& policy, bit_iterator<RandomIt> first,
     bit_iterator<RandomIt> last, Compare comp) {
-    (policy, first, last, comp); 
-    return true;
+    return std::is_heap(std::forward<ExecutionPolicy>(policy), first, last, comp);
 }
 
 // ========================================================================== //
