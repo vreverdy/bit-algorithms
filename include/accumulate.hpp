3b8a73708b77304d9d5cb99d418f5001849f6fd1
Collin Gress <gress126@gmail.com>
2019-05-08 15:28:53 -0500

    add discussion about accumulate

--- include/accumulate.hpp
+++ include/accumulate.hpp
@@ -3,7 +3,7 @@
 // Name: accumulate.hpp
 // Description: bit_iterator overloads for std::accumulate
 // Creator: Vincent Reverdy
-// Contributor(s): 
+// Contributor(s): Collin Gress [2019] 
 // License: BSD 3-Clause License
 // ========================================================================== //
 #ifndef _ACCUMULATE_HPP_INCLUDED
@@ -18,12 +18,21 @@
 
 namespace bit {
 
-// TODO
+/* [Will not implement] 
+ * After some discussion, we think the following method should be left unimplemented.
+ * This is due to the fact that normal numeric types are not exactly comparable to 
+ * bit values. It doesn't really make sense to allow the addition of type T
+ * and bit::bit_value. If a user wants to use accumulate for bit iterators, 
+ * they should use the other overload of accumulate which allows them to pass a
+ * binary operation which is capable of doing arithmetic between bit::bit_value and T.
+ * Alternatively, they can use bit::count (when accumulating via addition)
+ 
 template <class InputIt, class T>
 T accumulate(bit_iterator<InputIt> first, bit_iterator<InputIt> last, T init) {
-    (first, last);
-    return init;
+    return bit::count(first, last, bit1) + init; 
 }
+
+*/
 
 // TODO
 template <class InputIt, class T, class BinaryOperation>
