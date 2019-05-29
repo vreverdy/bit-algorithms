3967ab42d1a3eeadf646952cffd923d01be38346
Collin Gress <gress126@gmail.com>
2019-05-29 11:46:41 -0500

    Implement max_element function. Add padded_read and several other utility functions as well as associated tests.

--- include/max_element.hpp
+++ include/max_element.hpp
@@ -2,8 +2,8 @@
 // Project: The Experimental Bit Algorithms Library
 // Name: max_element.hpp
 // Description: bit_iterator overloads for std::max_element
-// Creator: Vincent Reverdy
-// Contributor(s): 
+// Creator: Vincent Reverdy 
+// Contributor(s): Collin Gress [2019] 
 // License: BSD 3-Clause License
 // ========================================================================== //
 #ifndef _MAX_ELEMENT_HPP_INCLUDED
@@ -21,11 +21,54 @@ namespace bit {
 
 
 
-// Status: to do
+// Status: complete
 template <class ForwardIt>
 constexpr bit_iterator<ForwardIt> max_element(bit_iterator<ForwardIt> first,
     bit_iterator<ForwardIt> last) {
-    (last);
+
+    using word_type = typename bit_iterator<ForwardIt>::word_type;
+    constexpr std::size_t num_digits = bit::binary_digits<word_type>::value; 
+
+    bit_iterator<ForwardIt> cursor = first;
+
+    // handle misaligned first word. for example ^00100^101
+    if (!_is_aligned_lsb(cursor)) {
+        word_type first_word = _padded_read(cursor, last, bit::bit0);
+        if (first_word > 0) {
+            std::size_t first_set_bit_position = _tzcnt(first_word);
+            return cursor + (first_set_bit_position - cursor.position());
+        }
+
+        if (_in_same_word(cursor, last) ||
+            _is_one_past_last_bit(last, cursor.base())) {
+            return first;
+        }
+
+        std::advance(cursor, num_digits - cursor.position());
+    }
+
+    // full words
+    while (!_in_same_word(cursor, last) ||
+        _is_one_past_last_bit(last, cursor.base())) {
+        word_type cur_word = *(cursor.base());
+        if (cur_word > 0) {
+            std::size_t cur_set_bit_position = _tzcnt(cur_word);
+            return cursor + (cur_set_bit_position - cursor.position());
+        }
+        std::advance(cursor, num_digits);
+    }
+
+    if (cursor == last) {
+        return first;
+    }
+
+    // final, incomplete word
+    word_type last_word = _padded_read(cursor, last, bit::bit0);
+    if (last_word > 0) {
+        word_type last_set_bit_position = _tzcnt(last_word);
+        return cursor + (last_set_bit_position - cursor.position());
+    }
+
     return first;
 }
 
