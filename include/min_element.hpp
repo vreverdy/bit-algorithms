274d3b9221a9b6499f1ecd9a3de93b48abbca80a
Collin Gress <gress126@gmail.com>
2019-06-03 14:31:09 -0500

    min_element implementation + tests

--- include/min_element.hpp
+++ include/min_element.hpp
@@ -25,7 +25,65 @@ namespace bit {
 template <class ForwardIt>
 constexpr bit_iterator<ForwardIt> min_element(bit_iterator<ForwardIt> first,
     bit_iterator<ForwardIt> last) {
-    (last);
+
+    using word_type = typename bit_iterator<ForwardIt>::word_type;
+    constexpr std::size_t num_digits = binary_digits<word_type>::value;
+
+    bit_iterator<ForwardIt> cursor = first;
+    word_type mask;
+    word_type all_ones = _all_ones();
+
+    // handle case ^1101^0101 and 1^101^0101
+    if (!_is_aligned_lsb(cursor)) {
+        // 1^101^0101
+        if (last.base() == cursor.base()) {
+            mask = _shift_towards_lsb(all_ones, num_digits - first.position()); 
+            mask |= _shift_towards_msb(all_ones, last.position());
+            // fill bits outside of [first, last) with 1s
+            word_type masked_first_word = *(cursor.base()) | mask;
+            if (masked_first_word < all_ones) {
+                std::size_t first_unset_bit_position = _tzcnt(static_cast<word_type>(~masked_first_word));
+                return cursor + (first_unset_bit_position - cursor.position());
+            }
+            return cursor; // no zeros in range
+        }
+
+        mask = _shift_towards_lsb(all_ones, num_digits - first.position());
+        word_type masked_first_word = *(cursor.base()) | mask; 
+
+        if (masked_first_word < all_ones) {
+            std::size_t first_unset_bit_position = _tzcnt(static_cast<word_type>(~masked_first_word));
+            return cursor + (first_unset_bit_position - cursor.position()); 
+        }
+
+        cursor = bit_iterator(std::next(cursor.base()));
+    }
+
+    ForwardIt word_cursor = cursor.base();
+
+    while (word_cursor != last.base()) {
+        word_type cur_word = *word_cursor;
+        if (cur_word < all_ones) {
+            std::size_t cur_unset_bit_position = _tzcnt(static_cast<word_type>(~cur_word));
+            return bit_iterator(word_cursor, cur_unset_bit_position);
+        }
+        ++word_cursor;
+    }
+
+    cursor = bit_iterator(word_cursor);
+
+    if (cursor == last) {
+        return first;
+    }
+
+    mask = _shift_towards_msb(all_ones, last.position());
+    word_type masked_last_word = *(cursor.base()) | mask; 
+
+    if (masked_last_word < all_ones) {
+      std::size_t last_set_bit_position = _tzcnt(static_cast<word_type>(~masked_last_word));
+      return cursor + last_set_bit_position;
+    }
+
     return first;
 }
 
